aec3bc67d376c4d12935440c89ddaa6e52f0242c
Vojtěch Červený <cervik49@gmail.com>
2020-04-21 02:06:06 +0200

    adding Instagram support

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Parallax from 'react-rellax'
 import { useMediaQuery } from 'react-responsive'
 
--- src/sections/Instagram.jsx
+++ src/sections/Instagram.jsx
@@ -1,16 +1,82 @@
-import React from 'react'
-import { H2, LightSection, Cell, Container, Image } from '../styles/style'
-import instagram1 from '../images/instagram1.jpg'
-import instagram2 from '../images/instagram2.jpg'
-import instagram3 from '../images/instagram3.jpg'
-import instagram4 from '../images/instagram4.jpg'
-import instagram6 from '../images/instagram6.jpg'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useMediaQuery } from 'react-responsive'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
+import {
+  H2,
+  LightSection,
+  Cell,
+  Container,
+  Image,
+  Column,
+  Row,
+} from '../styles/style'
+
 export default function Instagram() {
+  const [photos, setPhotos] = useState([])
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 1224px)',
+  })
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const URL = 'https://www.instagram.com/vkhostrava/'
+        const response = await axios.get(URL)
+        const instagramRegExp = new RegExp(
+          /<script type="text\/javascript">window\._sharedData = (.*);<\/script>/
+        )
+        const json = JSON.parse(response.data.match(instagramRegExp)[1])
+        const edges = json.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges.splice(
+          0,
+          8
+        )
+        const fetchedPhotos = edges.map(({ node }) => {
+          return {
+            url: `https://www.instagram.com/p/${node.shortcode}/`,
+            thumbnailUrl: node.thumbnail_src,
+            displayUrl: node.display_url,
+            caption: node.edge_media_to_caption.edges[0].node.text,
+          }
+        })
+        setPhotos(fetchedPhotos)
+      } catch (e) {
+        console.error('Fetching Instagram photos failed', e)
+      }
+    }
+    fetchData()
+  }, [])
+
+  const getGrid = () => {
+    return (
+      <Row>
+        {photos.map(photo => {
+          return (
+            <Column xs={12} md={6} lg={3} key={photo.url}>
+              <Image src={photo.displayUrl} />
+            </Column>
+          )
+        })}
+      </Row>
+    )
+  }
+
+  const getSlider = () => {
+    return (
+      <Slider {...settings}>
+        {photos.map(photo => {
+          return (
+            <Cell key={photo.url}>
+              <Image src={photo.displayUrl} />
+            </Cell>
+          )
+        })}
+      </Slider>
+    )
+  }
+
   var settings = {
     dots: true,
     infinite: true,
@@ -43,28 +109,11 @@ export default function Instagram() {
       },
     ],
   }
-
   return (
     <LightSection>
       <Container>
         <H2>Instagram</H2>
-        <Slider {...settings}>
-          <Cell>
-            <Image src={instagram1} />
-          </Cell>
-          <Cell>
-            <Image src={instagram2} />
-          </Cell>
-          <Cell>
-            <Image src={instagram3} />
-          </Cell>
-          <Cell>
-            <Image src={instagram4} />
-          </Cell>
-          <Cell>
-            <Image src={instagram6} />
-          </Cell>
-        </Slider>
+        {isDesktopOrLaptop ? getGrid() : getSlider()}
       </Container>
     </LightSection>
   )
