4a1393de51c27dda229eacdd3b50ae95e1da9908
Vojtěch Červený <cervik49@gmail.com>
2020-05-31 20:01:22 +0200

    change key for map

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -14,7 +14,7 @@ import Footer from '../../sections/Footer'
 const MyMapComponent = compose(
   withProps({
     googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBOPdamAuAzyllJJHoqRs6uA2Ezu5D-BIY',
+      'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDHKMmPYdY0LZR-IbxdKtFQiNVNRok0iXw',
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
